a3168d2e77d69283cfd6c557a716deeaab6711f8
ASTUDIOS | Development <64874485+aqade@users.noreply.github.com>
2023-04-11 09:42:26 +0200

    Initial upload

---
+++ shared/language.lua
@@ -0,0 +1,25 @@
+--------------------------------------
+--<!>-- ASTUDIOS | DEVELOPMENT --<!>--
+--------------------------------------
+-- Language settings
+Language = {
+    Error = {
+        ['no_camera'] = 'Camera doesn\'t exist',
+    },
+    Menu = {
+        ['cctvmenu'] = 'CCTV',
+        ['cctvmenuicon'] = 'fas fa-video',
+        ['cctvbank'] = 'CCTV | Banks',
+        ['cctvbankicon'] = 'fas fa-university',
+        ['cctvstores'] = 'CCTV | Stores',
+        ['cctvstoresicon'] = 'fas fa-store',
+        ['cctvother'] = 'CCTV | Other',
+        ['cctvothericon'] = 'fas fa-building',
+        ['exit'] = 'Return',
+        ['exiticon'] = 'fas fa-circle-arrow-left',
+    },
+    Targeting = {
+        ['cctvmenu'] = 'CCTV',
+        ['cctvmenuicon'] = 'fas fa-video',
+    },
+}
